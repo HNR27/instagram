d98a3521e77d8eb259e039298b51935160731ec6
HNR27 <30395313+HNR27@users.noreply.github.com>
2017-10-12 14:22:30 +0700

    Add files via upload

---
+++ XI-RPL2-18/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tipu Tipu Anti Crack</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="form">
+	<div class="content">
+		<h1>Instagram</h1>
+		<h2>___________________________</h2>
+		<h3>Sign up to see photos and videos<br>from your fiends</h3>
+		<a href="www.facebook.com"><button>Login with Facebook</button><br></a>
+		<form action= "view.php" method= "POST">
+		<p style="margin: 5px; color: black; font-family: billabong;">OR</p>
+		<input type="text" name="email" placeholder="Mobile Number or Email"><br>
+		<input type="text" name="full-name" placeholder="Full Name"><br>
+		<input type="text" name="username" placeholder="Uername"><br>
+		<input type="password" name="password" placeholder="Password"><br>
+		<button type="submit">Sign Up</button><br>
+		<p2>By signing up, you agree to our<br></p2>
+		<p2>Terms & Privacy Policy</p2>
+	</div>
+</center>
+</div>
+</form>
+</body>
+</html>
---
+++ XI-RPL2-18/view.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<p align="center"><font size="5px">Info Biodata Anda</p>
+<p align ="left"><table>
+<tr><td>Email Anda</td> <td>:</td><td><?php echo $_POST["email"];?></td></tr>
+<tr><td>Nama Anda</td> <td>:</td><td><?php echo $_POST["full-name"];?></td></tr>
+<tr><td>Username Anda</td> <td>:</td><td><?php echo $_POST["username"];?></td></tr>
+<tr><td>Password Anda</td> <td>:</td><td><?php echo $_POST["password"];?></td></tr>
+</table>
+</p>
+</body>
+</html>
