768b79721530d1585af0826646f95efc52696374
HNR27 <30395313+HNR27@users.noreply.github.com>
2017-10-23 07:36:32 +0700

    Update view.php

--- XI-RPL2-18/view.php
+++ XI-RPL2-18/view.php
@@ -1,12 +1,50 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Hasil</title>
+	<link rel="stylesheet" href="style.css">
+</head>
 <body>
-<p align="center"><font size="5px">Info Biodata Anda</p>
-<p align ="left"><table>
-<tr><td>Email Anda</td> <td>:</td><td><?php echo $_POST["email"];?></td></tr>
-<tr><td>Nama Anda</td> <td>:</td><td><?php echo $_POST["full-name"];?></td></tr>
-<tr><td>Username Anda</td> <td>:</td><td><?php echo $_POST["username"];?></td></tr>
-<tr><td>Password Anda</td> <td>:</td><td><?php echo $_POST["password"];?></td></tr>
+<p align="center"><font size="5px"><b>Registrasi Sukses</b></font></p>
+<table align="center">
+<?php 
+$host= "localhost";
+$user= "root";
+$password= "";
+$email=$_POST['email'];
+$fullname=$_POST['fullname'];
+$username=$_POST['username'];
+$passwd=$_POST['passwd'];
+{
+$conn=mysqli_connect ($host,$user,$password);
+mysqli_select_db ($conn,'ig');
+$sql="insert into tuser values ('$email', '$fullname', '$username', '$passwd')";
+$hasil=mysqli_query ($conn,$sql); 
+}
+?>
+<div id="container">
+	<div class="Header">
+		Verivication
+	</div>
+	<div class="hasil">
+		<div class="email">
+			Email : <?php echo" $email" ?><br>
+		</div>
+		<div class="fullname">
+			Full name : <?php echo "$fullname"; ?><br>
+		</div>
+		<div class="user">
+			Username  : <?php echo "$username"; ?><br>
+		</div>
+		<div class="password">
+			Password : <?php echo "$passwd"; ?><br>
+		</div>
+	</div>
+</div>
 </table>
-</p>
 </body>
 </html>
+
+
+
